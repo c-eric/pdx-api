e9f2aa1314fcff6c7b1935af398520674c735e88
c-eric <43305167+c-eric@users.noreply.github.com>
2024-02-18 02:59:05 -0500

    Initial commit

---
+++ src/main/java/com/example/palsapi/PalsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.palsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/palsapi/PalsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
