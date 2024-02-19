23a64b844c08bbb871cb4d6df33cf3f6e87e3ba3
c-eric <43305167+c-eric@users.noreply.github.com>
2024-02-19 05:54:29 -0500

    Added CrossOrigin and updated mapping

--- src/main/java/com/example/palsapi/PalsController.java
+++ src/main/java/com/example/palsapi/PalsController.java
@@ -3,6 +3,7 @@ package com.example.palsapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,21 +15,25 @@ public class PalsController {
     @Autowired
     private PalsService palsService;
 
-    @GetMapping
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
+    @GetMapping("/pals")
     public ResponseEntity<List<Pals>> getAllPals() {
         return new ResponseEntity<List<Pals>>(palsService.findAllPals(),HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/name/{name}")
     public ResponseEntity<List<Pals>> getPalsByName(@PathVariable String name) {
         return new ResponseEntity<List<Pals>>(palsService.findPalsByName(name), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/type/{typeName}")
     public ResponseEntity<List<Pals>> getPalsByType(@PathVariable String typeName) {
         return new ResponseEntity<List<Pals>>(palsService.findPalsByType(typeName), HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/suitability/{suitabilityName}")
     public ResponseEntity<List<Pals>> getPalsBySuitability(@PathVariable String suitabilityName) {
         return new ResponseEntity<List<Pals>>(palsService.findsPalsBySuitability(suitabilityName), HttpStatus.OK);
